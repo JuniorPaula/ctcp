39041d904c095ca524e317e56e051b756450bdbd
Junior Paula <luke.junnior@icloud.com>
2024-10-29 09:52:37 -0300

    Implements dequeu message Implements banned ip

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h> // for close
+#include<arpa/inet.h> // for inet_ntop()
 
 #define PORT 6969
 #define BUFFER_SIZE 64
 #define MAX_MESSAGES 100
+#define BAN_LIMIT 600.0
 
 typedef enum {
 	CLIENT_CONNECTED = 1,
@@ -22,6 +24,12 @@ typedef struct {
 	struct sockaddr_in addr;
 } Message;
 
+typedef struct BannedIP {
+  char ip[INET_ADDRSTRLEN];
+  time_t banned_at;
+  struct BannedIP *next;
+} BannedIP;
+
 typedef struct {
 	Message messages[MAX_MESSAGES];
 	int start;
@@ -31,8 +39,13 @@ typedef struct {
 } MessageQueue;
 
 MessageQueue message_queue;
+BannedIP *banned_ips_head = NULL;
+pthread_mutex_t banned_ips_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void init_message_queue(MessageQueue *queue);
+int dequeue_message(MessageQueue *queue, Message *message);
+int is_ip_banned(const char *ip, time_t *banned_at);
+void remove_banned_ip(const char *ip);
 void *server_thread(void *arg);
 
 int main()
@@ -91,15 +104,76 @@ void init_message_queue(MessageQueue *queue)
 void *server_thread(void *arg)
 {
   MessageQueue *queue = (MessageQueue *)arg;
-  printf("%d", queue->start);
+
+  while(1) {
+    Message msg;
+    dequeue_message(queue, &msg);
+    switch(msg.type) {
+      case CLIENT_CONNECTED: {
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(msg.addr.sin_addr), ip, INET_ADDRSTRLEN);
+        time_t banned_at;
+        int is_banned = is_ip_banned(ip, &banned_at);
+        time_t now = time(NULL);
+
+        if (is_banned) {
+          double elapsed = difftime(now, banned_at);
+          if (elapsed >= BAN_LIMIT) {
+            remove_banned_ip(ip);
+            is_banned = 0;
+          }
+        }
+      }
+    }
+  }
 }
 
+int dequeue_message(MessageQueue *queue, Message *message)
+{
+  pthread_mutex_lock(&queue->mutex);
+  while(queue->start == queue->end) {
+    pthread_cond_wait(&queue->cond, &queue->mutex);
+  }
 
+  *message = queue->messages[queue->start];
+  queue->start = (queue->start + 1) % MAX_MESSAGES;
+  pthread_mutex_unlock(&queue->mutex);
+  return 1;
+}
 
+int is_ip_banned(const char *ip, time_t *banned_at)
+{
+  pthread_mutex_lock(&banned_ips_mutex);
+  BannedIP *curr = banned_ips_head;
+  while(curr != NULL) {
+    if (strcmp(curr->ip, ip) == 0) {
+      *banned_at = curr->banned_at;
+      pthread_mutex_unlock(&banned_ips_mutex);
+      return 1;
+    }
+    curr = curr->next;
+  }
+  pthread_mutex_unlock(&banned_ips_mutex);
+  return 0;
+}
 
-
-
-
-
-
-
+void remove_banned_ip(const char *ip)
+{
+  pthread_mutex_lock(&banned_ips_mutex);
+  BannedIP *prev = NULL;
+  BannedIP *curr = banned_ips_head;
+  while(curr != NULL) {
+    if (strcmp(curr->ip, ip) == 0) {
+      if (prev == NULL) {
+        banned_ips_head = curr->next;
+      } else {
+        prev->next = curr->next;
+      }
+      free(curr);
+      break;
+    }
+    prev = curr;
+    curr = curr->next;
+  }
+  pthread_mutex_unlock(&banned_ips_mutex);
+}
